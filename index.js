50fd841ebdd8756c34da8204278ebd9b6f3673e7
Kaushik <47687763+Kaushik1181@users.noreply.github.com>
2020-08-13 10:41:40 +0530

    completed

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+"
 document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
 
 if(randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player One Won";
+  document.querySelector("h1").innerHTML = "Player One Won"; //if player one wins
 } else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player Two Won";
+  document.querySelector("h1").innerHTML = "Player Two Won"; //if player two wins
 } else {
-  document.querySelector("h1").innerHTML = "Draw";
+  document.querySelector("h1").innerHTML = "Draw"; // if match draw
 }
